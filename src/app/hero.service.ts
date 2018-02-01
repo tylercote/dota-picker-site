2ebe096ef3c07c75a9debedebfb769c022256675
Tyler Cote <adcbl8x@adcbl8x-mbp.eagleinvsys.com>
2018-02-01 12:04:35 -0500

    Commit from work

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,29 +1,46 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './model/hero';
 import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
 
 
 @Injectable()
 export class HeroService {
 
-  constructor(private http: HttpClient) { }
+  heroes: Hero[];
 
+  constructor(private http: HttpClient) { }
 
   /*
   Returns an array of all Hero objects with current matchups.
    */
   getHeroes(): Hero[] {
-    interface ItemsResponse {
-      results: string[];
-    }
-    var heroes = [];
-    this.http.get('/https://www.dotabuff.com/heroes').subscribe(data => {
-      this.results = data.results;
-    })
-    return heroes;
+    // this.http.get('/https://www.dotabuff.com/heroes').subscribe(data => data.json);
+    // this.heroes = ;
+    return;
   }
 
+  /*
+  Pushes the given hero to the global hero array. Makes available to pick.
+   */
+  pushHero(hero: Hero) {
+    if (!this.heroesContains(hero)) {
+      this.heroes.push(hero);
+    }
+  }
 
+  /*
+  Pops the given hero off the global hero array. Makes unavailable to pick.
+   */
+  popHero(heroToRemove: Hero) {
+    this.heroes = this.heroes.filter(hero => hero !== heroToRemove);
+  }
 
+  heroesContains(hero: Hero): boolean {
+    for (let i = 0; i < this.heroes.length; i++) {
+      if (this.heroes[i] === hero) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
--- src/app/model/team.ts
+++ src/app/model/team.ts
@@ -1,7 +1,28 @@
 import { Hero } from './hero';
+import { HeroService } from '../hero.service';
+
 export class Team {
   isRadiant: boolean;
+  // pool: Hero[];
   picks: Hero[];
   bans: Hero[];
   suggestedPicks: Map<Hero, number>;
+
+  constructor(isRadiant: boolean, private heroService: HeroService) {
+    this.isRadiant = isRadiant;
+    this.suggestedPicks = new Map<Hero, number>();
+    for (let i = 0; i < heroService.heroes.length; i++) {
+      this.suggestedPicks.
+    }
+  }
+
+  calcSuggestedPicks () {
+    // sort suggested picks by hero name alphabetically
+
+    for (let i = 0; i < 5; i++) {
+      if (this.picks[i] !== null) {
+        this.picks[i].matchups;
+      }
+    }
+  }
 }
